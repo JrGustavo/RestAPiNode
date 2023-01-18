d5b56b872b4beb434aa364496de9124c7cbe3439
JrGustavo <gus@soypetal.com>
2023-01-17 20:17:39 -0500

    Upload new code

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express()
 app.use(morgan('dev'))
 
 app.get('/products', (req, res) => {
+    res.send('Actualizando products')
+
+    app.get('/products', (req, res) => {
+        res.send('elimiando productos ')
+
+
+app.get('/products', (req, res) => {
     res.send('obteniendo products')
 })
 app.listen(3000)
