f626455dd137d1b43f701ee8cb3fe73ef33ee45e
JrGustavo <gus@soypetal.com>
2023-01-21 19:24:43 -0500

    Upload code

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-const express = require('express')
-const morgan = require('morgan')
+import express from "express";
+
+import morgan from "morgan";
 
 const app = express()
 
+const products = [ {
+    "id": "Albert",
+    "name": "Baran",
+    "age": "27"
+}]
 app.use(morgan('dev'))
+app.use(express.json(products))
 
-app.get('/products', (req, res) => {
-    res.send('Actualizando products')
-
-    app.get('/products', (req, res) => {
-        res.send('elimiando productos ')
-
-
-app.get('/products', (req, res) => {
-    res.send('obteniendo products')
+app.get('/products', (req, res) =>{
+    res.json(products)
 })
-app.listen(3000)
-console.log(`Server listening on port ${3000}`)
 
+app.get('/products', (req, res) =>{
+    res.send('Obteniendo products')
+})
+
+app.get('/products', (req, res) =>{
+    res.send('Obteniendo products')
+})
+
+app.get('/products', (req, res) =>{
+    res.send('Obteniendo products')
+})
+
+app.listen(3000)
+
+console.log(`server on port ${3000}`)
